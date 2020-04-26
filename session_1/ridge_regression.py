c0f0ac1d7c6dd5694c440f8dee31148250c41873
dongchau <phuctj123@gmail.com>
2020-04-26 18:12:56 +0700

    update code comment

--- session_1/ridge_regression.py
+++ session_1/ridge_regression.py
@@ -2,6 +2,15 @@ import numpy as np
 
 
 def load_data(data_path):
+    """
+    Đọc dữ liệu từ file death_rates-data.txt
+    :param data_path: đường dẫn đến file
+    :return:
+        X: mảng 2 chiều biểu diễn tập thuộc tính
+        mỗi hàng tương ứng với 1 điểm dữ liệu
+        Y: mảng 1 chiều biểu diễn các giá trị
+        death rate tương ứng với từng điểm dữ liệu
+    """
     with open(data_path) as file:
         data = np.loadtxt(file)
         X = data[:, 1:16]
@@ -10,6 +19,12 @@ def load_data(data_path):
 
 
 def normalize_and_add_ones(X):
+    """
+    Chuẩn hóa dữ liệu sử dụng feature scaling
+    :param X: Tập thuộc tính đầu vào
+    :return: tập thuộc tính đã chuẩn hóa và thêm vector cột [1 1 ... 1] vào
+        đầu (để nhân với bias w0)
+    """
     X_max = np.array([[np.amax(X[:, column_id])
                        for column_id in range(X.shape[1])]
                       for _ in range(X.shape[0])])
@@ -28,6 +43,13 @@ class RidgeRegression:
         return
 
     def fit(self, X_train, Y_train, LAMBDA):
+        """
+        Tìm nghiệm bằng công thức nghiệm 𝑤∗=(𝑋^𝑇 * 𝑋 + 𝜆 * 𝐼_(𝐾+1))^(−1) * 𝑋𝑌
+        :param X_train: tập thuộc tính
+        :param Y_train: tập giá trị đầu ra tương ứng với từng vector thuộc tính
+        :param LAMBDA: weight decay, siêu tham số, 0 < LAMBDA < 1
+        :return: vector tham số W
+        """
         assert len(X_train.shape) == 2 and X_train.shape[0] == Y_train.shape[0]
         W = np.linalg.inv(np.transpose(X_train).dot(X_train) + \
                           LAMBDA * np.identity(X_train.shape[1])
@@ -35,20 +57,37 @@ class RidgeRegression:
         return W
 
     def fit_gradient_descent(self, X_train, Y_train, LAMBDA, lr, max_num_epochs=100, batch_size=128):
+        """
+        Tìm nghiệm bằng thuật toán gradient
+        :param X_train: tập thuộc tính
+        :param Y_train: tập giá trị đầu ra tương ứng với từng vector thuộc tính
+        :param LAMBDA: weight decay, siêu tham số
+        :param lr: learning rate, siêu tham số
+        :param max_num_epochs: số lượng epoch khi train
+        :param batch_size: kích thước batch
+        :return: vector tham số W
+        """
         W = np.random.rand(X_train.shape[1])
+        W = W.reshape((X_train.shape[1], 1))
         lass_lost = 10e+8
         for epoch in range(max_num_epochs):
+            # xáo trộn tập dữ liệu khi bắt đầu 1 epoch mới
             arr = np.array(range(X_train.shape[0]))
             np.random.shuffle(arr)
             X_train = X_train[arr]
             Y_train = Y_train[arr]
+            # số batch trên 1 epoch
             total_minibatch = int(np.ceil(X_train.shape[0] / batch_size))
             for i in range(total_minibatch):
                 index = i * batch_size
                 X_train_sub = X_train[index:index + batch_size]
                 Y_train_sub = Y_train[index:index + batch_size]
+                # tính gradient của hàm loss
                 grad = np.transpose(X_train_sub).dot(X_train_sub.dot(W) - Y_train_sub) + LAMBDA * W
+                # cập nhật trọng số W
                 W = W - lr * grad
+            # điều kiện dừng: sau mỗi epoch nếu giá trị thay đổi của hàm loss
+            # không quá 10^-5 thì kết thúc quá trình train
             new_loss = self.compute_RSS(self.predict(W, X_train), Y_train)
             if np.abs(new_loss - lass_lost) <= 1e-5:
                 break
@@ -56,26 +95,49 @@ class RidgeRegression:
         return W
 
     def predict(self, W, X_new):
+        """
+        Đưa ra dự đoán gái trị death rate Y ứng với trọng số W và đầu vào X
+        :param W: trọng số của mô hình
+        :param X_new: giá trị input
+        :return: giá trị dự đoán của Y
+        """
         return np.array(X_new).dot(W)
 
     def compute_RSS(self, Y_new, Y_predicted):
+        """
+        Tính giá trị hàm loss
+        :param Y_new: giá trị Y thực tế
+        :param Y_predicted: Giá trị Y dự đoán bởi mô hình
+        :return: Giá trị hàm MSE loss
+        """
         return 1.0 / Y_new.shape[0] * np.sum((Y_new - Y_predicted) ** 2)
 
     def get_the_best_LAMBDA(self, X_train, Y_train):
+        """
+        Lựa chọn tham số Lambda bằng phương pháp k-fold cross validation
+        :param X_train, Y_train: Tập các điểm dữ liệu huấn luyện và nhãn
+        :return: giá trị Lambda làm cho hàm loss đạt giá trị nhỏ nhất trên
+            tập huấn luyện
+        """
+
         def cross_validation(num_folds, LAMBDA):
+            # chia tập dữ liệu huấn luyện thành k phần
             row_ids = np.array(range(X_train.shape[0]))
-            valid_ids = np.split(row_ids[:len(row_ids) - len(row_ids) % num_folds], num_folds)
+            valid_ids = np.split(row_ids[:(len(row_ids) - len(row_ids) % num_folds)], num_folds)
             valid_ids[-1] = np.append(valid_ids[-1], row_ids[len(row_ids) - len(row_ids) % num_folds:])
             train_ids = [[k for k in row_ids if k not in valid_ids[i]] for i in range(num_folds)]
             aver_RSS = 0
             for i in range(num_folds):
+                # với mỗi fold lấy 1 phần làm tập val. k - 1 phần làm tập train
                 valid_part = {'X': X_train[valid_ids[i]], 'Y': Y_train[valid_ids[i]]}
                 train_part = {'X': X_train[train_ids[i]], 'Y': Y_train[train_ids[i]]}
+                # đưa ra giá trị dự đoán và tính hàm loss`
                 W = self.fit(train_part['X'], train_part['Y'], LAMBDA)
                 Y_predict = self.predict(W, valid_part['X'])
                 aver_RSS += self.compute_RSS(valid_part['Y'], Y_predict)
             return aver_RSS / num_folds
 
+        # chọn Lambda có giá trị loss thấp nhất
         def range_scan(best_LAMBDA, minimum_RSS, LAMBDA_values):
             for current_LAMBDA in LAMBDA_values:
                 aver_RSS = cross_validation(num_folds=5, LAMBDA=current_LAMBDA)
@@ -84,26 +146,87 @@ class RidgeRegression:
                     minimum_RSS = aver_RSS
             return best_LAMBDA, minimum_RSS
 
-        best_LAMBDA, minimum_RSS = range_scan(best_LAMBDA=0, minimum_RSS=10000 ** 2, LAMBDA_values=range(50))
-
-        LAMBDA_values = [k * 1.0 / 1000 for k in range(
-            max(0, (best_LAMBDA - 1) * 1000), (best_LAMBDA + 1) * 1000, 1)]
-
-        best_LAMBDA, minnimum_RSS = range_scan(best_LAMBDA=best_LAMBDA, minimum_RSS=minimum_RSS, LAMBDA_values=LAMBDA_values)
+        # chạy lần 1 để chọn cận trêm của hàm loss
+        best_LAMBDA, minimum_RSS = range_scan(best_LAMBDA=0,
+                                              minimum_RSS=10000 ** 2,
+                                              LAMBDA_values=[i * 1.0 / 1000 for i in range(1, 1000, 1)])
+        # print('Best LAMBDA: ', best_LAMBDA)
+        # print('Minimum RSS: ', minimum_RSS)
+        # chạy lần 2 để đưa ra giá trị Lambda tốt nhất
+        best_LAMBDA, minimum_RSS = range_scan(best_LAMBDA=best_LAMBDA,
+                                              minimum_RSS=minimum_RSS,
+                                              LAMBDA_values=[i * 5.0 / 10000 for i in range(1, 2000, 1)])
 
         return best_LAMBDA
+
+    def get_the_best_lr(self, X_train, Y_train, LAMBDA):
+        """
+        Lựa chọn learning rate bằng phương pháp k-fold cross validation
+        :param X_train, Y_train: Tập các điểm dữ liệu huấn luyện và nhãn
+        :param LAMBDA:
+        :return: giá trị Lambda làm cho hàm loss đạt giá trị nhỏ nhất trên
+            tập huấn luyện
+        """
+
+        def cross_validation(num_folds, lr):
+            # chia tập dữ liệu huấn luyện thành k phần
+            row_ids = np.array(range(X_train.shape[0]))
+            valid_ids = np.split(row_ids[:(len(row_ids) - len(row_ids) % num_folds)], num_folds)
+            valid_ids[-1] = np.append(valid_ids[-1], row_ids[len(row_ids) - len(row_ids) % num_folds:])
+            train_ids = [[k for k in row_ids if k not in valid_ids[i]] for i in range(num_folds)]
+            aver_RSS = 0
+            for i in range(num_folds):
+                # với mỗi fold lấy 1 phần làm tập val. k - 1 phần làm tập train
+                valid_part = {'X': X_train[valid_ids[i]], 'Y': Y_train[valid_ids[i]]}
+                train_part = {'X': X_train[train_ids[i]], 'Y': Y_train[train_ids[i]]}
+                # đưa ra giá trị dự đoán và tính hàm loss`
+                W = self.fit_gradient_descent(train_part['X'], train_part['Y'], LAMBDA, lr)
+                Y_predict = self.predict(W, valid_part['X'])
+                aver_RSS += self.compute_RSS(valid_part['Y'], Y_predict)
+            return aver_RSS / num_folds
+
+        # chọn Lambda có giá trị loss thấp nhất
+        def range_scan(best_lr, minimum_RSS, lr_values):
+            for current_lr in lr_values:
+                aver_RSS = cross_validation(num_folds=5, lr=current_lr)
+                if aver_RSS < minimum_RSS:
+                    best_lr = current_lr
+                    minimum_RSS = aver_RSS
+            return best_lr, minimum_RSS
+
+        # chạy lần 1 để chọn cận trêm của hàm loss
+        best_lr, minimum_RSS = range_scan(best_lr=0,
+                                          minimum_RSS=10000 ** 2,
+                                          lr_values=[i * 1.0 / 1000 for i in range(1, 1000, 1)])
+        # print('Best lr: ', best_lr)
+        # print('Minimum RSS: ', minimum_RSS)
+        # chạy lần 2 để đưa ra giá trị lr tốt nhất
+        # best_lr, minimum_RSS = range_scan(best_lr=best_lr,
+        #                                   minimum_RSS=minimum_RSS,
+        #                                   lr_values=[i * 5.0 / 10000 for i in range(1, 2000, 1)])
+
+        return best_lr
 
 
 if __name__ == '__main__':
     X, Y = load_data(data_path='datasets/death-rates-data.txt')
     X = normalize_and_add_ones(X)
+    # chia tập dữ liệu thành train và test
     X_train, Y_train = X[:50], Y[:50]
     X_test, Y_test = X[50:], Y[50:]
 
     ridge_regression = RidgeRegression()
-    best_LAMBDA = ridge_regression.get_the_best_LAMBDA(X_train, Y_train)
-    print('Best LAMBDA: ', best_LAMBDA)
-    W_learned = ridge_regression.fit(X_train, Y_train, best_LAMBDA)
+    LAMBDA = ridge_regression.get_the_best_LAMBDA(X_train, Y_train)
+    print('Best LAMBDA: ', LAMBDA)
+    # sử dụng công thức nghiệm
+    W_learned = ridge_regression.fit(X_train, Y_train, LAMBDA)
     Y_predicted = ridge_regression.predict(W=W_learned, X_new=X_test)
+    print('Loss khi dùng công thức nghiệm', ridge_regression.compute_RSS(Y_new=Y_test, Y_predicted=Y_predicted))
 
-    print(ridge_regression.compute_RSS(Y_new=Y_test, Y_predicted=Y_predicted))
+    # sử dụng phương pháp gradient
+    lr = ridge_regression.get_the_best_lr(X_train, Y_train, LAMBDA)
+    print('Best learning rate: ', lr)
+    W_gradient = ridge_regression.fit_gradient_descent(X_train, Y_train, LAMBDA, lr, batch_size=4)
+    Y_predicted_gradient = ridge_regression.predict(W=W_gradient, X_new=X_test)
+
+    print('Loss khi dùng gradient', ridge_regression.compute_RSS(Y_new=Y_test, Y_predicted=Y_predicted_gradient))
--- session_1/tf_idf.py
+++ session_1/tf_idf.py
@@ -5,7 +5,13 @@ import re
 from collections import defaultdict
 from nltk.corpus import stopwords
 
+
 def gather_20newsgroups_data():
+    """
+    Thu thập và chuẩn hóa dữ liệu
+    :return:
+    """
+    # đường dẫn đến thư mục chứa dữ liệu
     path = 'datasets/20news-bydate/'
     # dirs = [path + dir_name + '/' for dir_name in os.listdir(path) if not os.path.isfile(path + dir_name)]
     # train_dir, test_dir = (dirs[0], dirs[1] if 'train' in dirs[0] else (dirs[1], dirs[0]))
@@ -18,6 +24,13 @@ def gather_20newsgroups_data():
     stemmer = PorterStemmer()
 
     def collect_data_from(parent_dir, newsgroup_list):
+        """
+        Đọc và xử lí dữ liệu
+        :param parent_dir: 1 xâu biểu diễn đường dẫn tới thư mục chứa dữ liệu
+        :param newsgroup_list: 1 list biểu diễn danh sách thư mục con
+        :return: 1 list dữ liệu đã được chuẩn hóa, mỗi phần tử của list là một
+            xâu có dạng "nhãn lớp <fff> tên file chứa dữ liệu <fff> nội dung văn bản"
+        """
         data = []
         for group_id, newsgroup in enumerate(newsgroup_list):
             label = group_id
@@ -28,7 +41,9 @@ def gather_20newsgroups_data():
             files.sort()
             for filename, filepath in files:
                 with open(filepath) as f:
+                    # đọc dữ liệu từ file
                     text = f.read().lower()
+                    # loại bỏ stop words và đưa các từ về dạng nguyên thủy
                     words = [stemmer.stem(word)
                              for word in re.split('\W+', text)
                              if word not in stop_words]
@@ -40,6 +55,7 @@ def gather_20newsgroups_data():
     train_data = collect_data_from(parent_dir=train_dir, newsgroup_list=list_newsgroups)
     test_data = collect_data_from(parent_dir=test_dir, newsgroup_list=list_newsgroups)
 
+    # xuất dữ liệu ra file
     full_data = train_data + test_data
     with open('datasets/20news-bydate/20news-train-processed.txt', 'w') as f:
         f.write('\n'.join(train_data))
@@ -52,15 +68,28 @@ def gather_20newsgroups_data():
 
 
 def generate_vocabulary(data_path):
+    """
+    Tạo từ điển
+    :param data_path: đường dẫn đến file văn bản
+    :return: ghi file từ điển ra file
+    """
     def compute_idf(df, corpus_size):
+        """
+        Tính idf của từ x
+        :param df: tổng số văn bản có chứa từ x
+        :param corpus_size: tổng số văn bản trong tập dữ liêu
+        :return: idf của từ x
+        """
         assert df > 0
         return np.log10(corpus_size * 1.0 / df)
 
+    # đọc dữ liệu từ file và đếm số văn bản
     with open(data_path) as f:
         lines = f.read().splitlines()
     doc_count = defaultdict(int)
     corpus_size = len(lines)
 
+    # ghi lại các từ trong văn bản và số lần xuất hiện của chúng
     for line in lines:
         features = line.split('<fff>')
         text = features[-1]
@@ -68,6 +97,7 @@ def generate_vocabulary(data_path):
         for word in words:
             doc_count[word] += 1
 
+    # tính idf của các từ xuất hiện trong trên 10 văn bản
     words_idfs = [(word, compute_idf(document_freq, corpus_size))
                   for word, document_freq in zip(doc_count.keys(), doc_count.values())
                   if document_freq > 10 and not word.isdigit()]
@@ -78,10 +108,15 @@ def generate_vocabulary(data_path):
 
 
 def get_tf_idf(data_path):
+    """
+    Tính tf-idf của từng từ trong từ điển
+    :param data_path: đường dẫn tới file từ điển
+    :return: tạo file chứa các từ trong từ điển và giá trị tf-idf tương ứng của từ đó
+    """
+    # đọc từ điển từ file
     with open('datasets/20news-bydate/words_idfs.txt') as f:
         words_idfs = [(line.split('<fff>')[0], float(line.split('<fff>')[1]))
                       for line in f.read().splitlines()]
-
         word_IDs = dict([(word, index) for index, (word, idf) in enumerate(words_idfs)])
         idfs = dict(words_idfs)
 
@@ -89,7 +124,7 @@ def get_tf_idf(data_path):
         documents = [(int(line.split('<fff>')[0]),
                      int(line.split('<fff>')[0]),
                      line.split('<fff>')[2])
-                    for line in f.read().splitlines()]
+                     for line in f.read().splitlines()]
 
     data_tf_idf = []
     for document in documents:
@@ -99,12 +134,15 @@ def get_tf_idf(data_path):
         max_term_freq = max([words.count(word) for word in word_set])
         words_tfidfs = []
         sum_squares = 0.0
+        # tính tf-idf cho từng từ
         for word in word_set:
             term_freq = words.count(word)
             tf_idf_value = term_freq * 1.0 / max_term_freq * idfs[word]
             words_tfidfs.append((word_IDs[word], tf_idf_value))
             sum_squares += tf_idf_value ** 2
 
+        # chuẩn hóa từng chiều của vector đặc trưng cho từng văn bản
+        # về đoạn [0, 1]
         words_tfidfs_normalized = [str(index) + ':' +
                                    str(tf_idf_value / np.sqrt(sum_squares))
                                    for index, tf_idf_value in words_tfidfs]
@@ -112,6 +150,7 @@ def get_tf_idf(data_path):
         sparse_rep = ' '.join(words_tfidfs_normalized)
         data_tf_idf.append((label, doc_id, sparse_rep))
 
+    # ghi dữ liệu ra file
     with open('datasets/20news-bydate/data_tf_idf.txt', 'w') as f:
         f.write('\n'.join([str(label) + '<fff>' + str(doc_id) + '<fff>' + sparse_rep
                            for label, doc_id, sparse_rep in data_tf_idf]))
@@ -119,5 +158,5 @@ def get_tf_idf(data_path):
 
 if __name__ == '__main__':
     gather_20newsgroups_data()
-    generate_vocabulary('datasets/20news-bydate/20news-full-processed.txt')
+    generate_vocabulary('datasets/20news-bydate/20news-train-processed.txt')
     get_tf_idf('datasets/20news-bydate/20news-train-processed.txt')
