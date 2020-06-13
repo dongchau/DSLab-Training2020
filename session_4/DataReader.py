a6ba8761d83f3cd4849769a59c766fbb5c6ad29a
dongchau <phuctj123@gmail.com>
2020-06-13 08:34:24 +0700

    add code comment

--- session_4/DataReader.py
+++ session_4/DataReader.py
@@ -3,42 +3,51 @@ import numpy as np
 
 
 class DataReader:
-    def __init__(self, data_path, batch_size, vocab_size):
+    """
+    Read data in batch
+    """
+    def __init__(self, data_path, batch_size):
         self.batch_size = batch_size
         with open(data_path) as f:
-            d_lines = f.readlines()
+            d_lines = f.read().splitlines()
 
         self.data = []
         self.labels = []
+        self.tokens = []
+        self.sentence_lengths = []
+        self.current_part = 0
+        # read data from file
         for data_id, line in enumerate(d_lines):
-            vector = [0.0 for _ in range(vocab_size)]
             features = line.split('<fff>')
-            label, doc_id = int(features[0]), int(features[1])
-            tokens = features[2].split()
-            for token in tokens:
-                index = int(token.split(':')[0])
-                value = float(token.split(':')[1])
-                vector[index] = value
+            label, doc_id, sentence_length = int(features[0]), int(features[1]), int(features[2])
+            vector = np.array([int(feature) for feature in features[3].split()])
+            # print(vector)
             self.data.append(vector)
+            self.tokens.append(vector[:sentence_length])
             self.labels.append(label)
+            self.sentence_lengths.append(sentence_length)
 
         self.data = np.array(self.data)
         self.labels = np.array(self.labels)
+        self.sentence_lengths = np.array(self.sentence_lengths)
+        self.tokens = np.array(self.tokens)
         self.num_epoch = 0
         self.batch_id = 0
 
-    def next_batch(self):
+    def next_batch(self, random_seed):
         start = self.batch_id * self.batch_size
         end = start + self.batch_size
         self.batch_id += 1
 
+        # create a batch
         if end + self.batch_size > len(self.data):
             end = len(self.data)
             self.num_epoch += 1
             self.batch_id = 0
             indices = list(range(len(self.data)))
-            random.seed(2018)
+            random.seed(random_seed)
             random.shuffle(indices)
-            self.data, self.labels = self.data[indices], self.labels[indices]
+            self.data, self.labels, self.tokens, self.sentence_lengths \
+                = self.data[indices], self.labels[indices], self.tokens[indices], self.sentence_lengths[indices]
 
-        return self.data[start:end], self.labels[start:end]
+        return self.data[start:end], self.labels[start:end], self.sentence_lengths[start:end], self.tokens[start:end]
--- session_4/rnn.py
+++ session_4/rnn.py
@@ -1,10 +1,13 @@
 import numpy as np
 import tensorflow.compat.v1 as tf
+from DataReader import DataReader
+
 tf.disable_eager_execution()
 
 
 MAX_DOC_LENGTH = 500
 NUM_CLASSES = 20
+RANDOM_SEED = 42
 
 
 class RNN:
@@ -24,7 +27,7 @@ class RNN:
 
     def embedding_layer(self, indices):
         pretrained_vectors = [np.zeros(self.embedding_size)]
-        np.random.seed(42)
+        np.random.seed(RANDOM_SEED)
         for _ in range(self.vocab_size + 1):
             pretrained_vectors.append(np.random.normal(loc=0., scale=1., size=self.embedding_size))
 
@@ -35,12 +38,12 @@ class RNN:
             shape=(self.vocab_size + 2, self.embedding_size),
             initializer=tf.constant_initializer(pretrained_vectors)
         )
-        return tf.nn.embedding_layer(embedding_matrix, indices)
+        return tf.nn.embedding_lookup(embedding_matrix, indices)
 
     def LSTM_layer(self, embeddings):
-        lstm_cell = tf.contrib.rnn.BasicLSTMCell(self.lstm_size)
+        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(self.lstm_size)
         zero_state = tf.zeros(shape=(self.batch_size, self.lstm_size))
-        initial_state = tf.contrib.rnn.LSTMStateTuple(zero_state, zero_state)
+        initial_state = tf.nn.rnn_cell.LSTMStateTuple(zero_state, zero_state)
 
         lstm_inputs = tf.unstack(tf.transpose(embeddings, perm=[1, 0, 2]))
         lstm_outputs, lstm_state = tf.nn.static_rnn(
@@ -80,12 +83,12 @@ class RNN:
         weights = tf.get_variable(
             name='final_layer_weights',
             shape=(self.lstm_size, NUM_CLASSES),
-            initializer=tf.random_normal_initializer(seed=42)
+            initializer=tf.random_normal_initializer(seed=RANDOM_SEED)
         )
         biases = tf.get_variable(
             name='final_layer_bias',
             shape=(self.lstm_size, NUM_CLASSES),
-            initializer=tf.random_normal_initializer(seed=42)
+            initializer=tf.random_normal_initializer(seed=RANDOM_SEED)
         )
         logits = tf.matmul(lstm_outputs, weights) + biases
 
@@ -102,7 +105,7 @@ class RNN:
         loss = tf.reduce_mean(loss)
 
         probs = tf.nn.softmax(logits)
-        predicted_labels = tf.argmax(probs, axits=1)
+        predicted_labels = tf.argmax(probs, axis=1)
         predicted_labels = tf.squeeze(predicted_labels)
         return predicted_labels, loss
 
@@ -113,18 +116,71 @@ class RNN:
 
 
 def train_and_evaluate_RNN():
-    with open('../datasets/w2v/vocab_w2v.txt') as f:
+    with open('../datasets/w2v/vocab-raw.txt') as f:
         vocab_size = len(f.readlines())
 
-    tf.set_random_seed(42)
+    tf.set_random_seed(RANDOM_SEED)
     rnn = RNN(
         vocab_size=vocab_size,
         embedding_size=300,
-        lstm_size=50,
+        lstm_size=32,
         batch_size=32
     )
     predicted_labels, loss = rnn.build_graph()
-    train = rnn.trainer(loss=loss, learning_rate=0.01)
+    train_op = rnn.trainer(loss=loss, learning_rate=0.01)
 
     with tf.Session() as sess:
-        train_data_reader = Da
+        train_data_reader = DataReader(
+            data_path='../datasets/w2v/20news-train-encoded.txt',
+            batch_size=32
+        )
+        test_data_reader = DataReader(
+            data_path='../datasets/w2v/20news-test-encoded.txt',
+            batch_size=32
+        )
+        step = 0
+        MAX_STEP = 10000
+
+        sess.run(tf.global_variables_initializer())
+        while step < MAX_STEP:
+            next_train_batch = train_data_reader.next_batch(random_seed=RANDOM_SEED)
+            train_data, train_labels, train_sentence_lengths, train_final_tokens = next_train_batch
+            plabels_eval, loss_eval, _ = sess.run(
+                [predicted_labels, loss, train_op],
+                feed_dict={
+                    rnn.data: train_data,
+                    rnn.labels: train_labels,
+                    rnn.sentence_lengths: train_sentence_lengths,
+                    rnn.final_tokens: train_final_tokens
+                }
+            )
+            step += 1
+            if step % 20 == 0:
+                print('Loss: ', loss_eval)
+
+            if train_data_reader.batch_id == 0:
+                num_true_preds = 0
+                while True:
+                    next_test_batch = test_data_reader.next_batch(random_seed=RANDOM_SEED)
+                    test_data, test_labels, test_sentence_lengths, test_final_tokens = next_test_batch
+                    test_plabels_eval = sess.run(
+                        predicted_labels,
+                        feed_dict={
+                            rnn.data: test_data,
+                            rnn.labels: test_labels,
+                            rnn.sentence_lengths: test_sentence_lengths,
+                            rnn.final_tokens: test_final_tokens
+                        }
+                    )
+                    matches = np.equal(test_plabels_eval, test_labels)
+                    num_true_preds += np.sum(matches.astype(float))
+
+                    if test_data_reader.batch_id == 0:
+                        break
+
+                print("Epoch: ", train_data_reader.num_epoch)
+                print("Accuracy on test data: ", num_true_preds * 100. / len(test_data_reader.data))
+
+
+if __name__ == '__main__':
+    train_and_evaluate_RNN()
